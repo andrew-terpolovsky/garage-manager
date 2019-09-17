215d2cca6d45de2024afa2dd1e401058f4e5197c
andrew_t <andrew.t@scopicsoftware.com>
2019-09-17 10:55:49 +0300

    Add default login URL.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+# Setup login URL
+LOGIN_URL = 'admin:login'
+
 # Rest Framework default permissions
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
