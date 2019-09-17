9c0c76105861d7e5dc045d1bf71d4cfa7131ca2b
andrew_t <andrew.t@scopicsoftware.com>
2019-09-17 10:39:35 +0300

    Register models in admin.

---
+++ backend/api/manager/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from .models import Manufacturer, Car, Garage
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        'manufacturer', 'model', 'year', 'color',
+    )
+
+
+@admin.register(Garage)
+class GarageAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'cars_amount',
+    )
