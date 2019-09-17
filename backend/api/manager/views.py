3a2d118f14f3a9684cc966cd8b211cf2c1c5adb3
andrew_t <andrew.t@scopicsoftware.com>
2019-09-17 10:51:47 +0300

    Add custom permissions class to disallow admin user delete and create cars.

--- backend/api/manager/views.py
+++ backend/api/manager/views.py
@@ -1,12 +1,33 @@
-from rest_framework import viewsets
+from django.utils.translation import gettext_lazy as _
+from rest_framework import permissions, viewsets
 
 from .models import Car, Garage
 from .serializers import CarSerializer, GarageSerializer
 
 
+class CarCreateDeletePermission(permissions.IsAdminUser):
+    """
+    Custom permissions class for allowing
+    adding and deleting cars only to superuser.
+    """
+    message = _('Only superuser can add or delete car.')
+    PERMS_MAP = ['PUT', 'PATCH']
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_superuser:
+            return True
+        if request.method in self.PERMS_MAP:
+            return True
+
+        return False
+
+
 class CarsViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    permission_classes = (CarCreateDeletePermission,)
 
 
 class GaragesViewSet(viewsets.ModelViewSet):
